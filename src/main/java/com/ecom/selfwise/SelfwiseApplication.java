7b3fcfce327d9b688206da563d82989ba8b06389
Nikhil <113880516+spiritboy47@users.noreply.github.com>
2026-05-12 11:51:38 +0530

    First commit

---
+++ src/main/java/com/ecom/selfwise/SelfwiseApplication.java
@@ -0,0 +1,14 @@
+package com.ecom.selfwise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelfwiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SelfwiseApplication.class, args);
+	}
+
+}
+
